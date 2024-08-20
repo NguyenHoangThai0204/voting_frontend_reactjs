10d34840eb8d43c029450cb772e8dae4ce267225
NguyenHoangThai0204 <thaidp2002@gmail.com>
2024-08-20 09:32:37 +0700

    done

--- src/component/Home/HeaderHome/HeaderHomeLoggedin/FormMenuSetting/FormMenuSetting.tsx
+++ src/component/Home/HeaderHome/HeaderHomeLoggedin/FormMenuSetting/FormMenuSetting.tsx
@@ -1,13 +1,36 @@
-
-  export function FormMenuSetting() {
+import "./FormMenuSetting.css"
+export function FormMenuSetting() {
    
-    return (
-      <div className="menu_layout_setting">
-        <ul className="menu_content">
-            <li>Personal page</li>
-            <li>Setting</li>
-            <li>Log out</li>
-        </ul>
-    </div>
-    );
-  }
+  const listItemMenuSetting = [
+    {name: "Personal page",
+      link: "/personal-page"
+    },
+    {name: "Setting",
+      link: "/setting"
+    },
+    {name: "Log out",
+      link: "/"
+    }
+  ];
+
+
+  return (
+    <div className="menu_layout_setting">
+      <ul className="menu_content">
+       
+        {
+          
+          listItemMenuSetting.map((item, index) => (
+            <li key={index}>
+                {item.name}
+            </li>
+          ))
+
+        }
+
+
+
+      </ul>
+  </div>
+  );
+}
