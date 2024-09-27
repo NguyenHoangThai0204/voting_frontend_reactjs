bebedbaedcba05a20f02c4d5f8ade5bf5bc40a95
NguyenHoangThai0204 <thaidp2002@gmail.com>
2024-09-27 13:30:42 +0700

    done

--- src/component/Admin/PollsManagement/PollsManagement.tsx
+++ src/component/Admin/PollsManagement/PollsManagement.tsx
@@ -1,24 +1,24 @@
 
-// import { useState, useEffect } from 'react';
-// import {getAllUser} from '../../../api/CallApi';
-// import { User } from '../../../typeObject';
+import { useState, useEffect } from 'react';
+import { getAllVotes } from '../../../api/CallApi';
+import { Poll } from '../../../typeObject';
 import '../UsersManagement/UsersManagement.css';
 
 export const PollsManagement = () => {
 
-  // const [users, setUsers] = useState<User[]>([]);
+  const [polls , setPolls] = useState<Poll[]>([]);
   
-  // useEffect(() => {
-  //   const fetchUsers = async () => {
-  //     try {
-  //       const response = await getAllUser();
-  //       setUsers(Array.isArray(response.data) ? response.data : [response.data]);
-  //     } catch (error) {
-  //       console.error("Error fetching users data:", error);
-  //     }
-  //   };
-  //   fetchUsers();
-  // }, []);
+  useEffect(() => {
+    const fetchPolls = async () => {
+      try {
+        const response = await getAllVotes();
+        setPolls(Array.isArray(response.data) ? response.data : [response.data]);
+      } catch (error) {
+        console.error("Error fetching users data:", error);
+      }
+    };
+    fetchPolls();
+  }, []);
 
   return (
     <div>
@@ -29,15 +29,22 @@ export const PollsManagement = () => {
                 <th scope="col">Poll name</th>
                 <th scope="col">Avatar</th>
                 <th scope="col">Description</th>
-                <th scope="col">Gender</th>
-                <th scope="col">Email</th>
-                <th scope="col">Address</th>
-                <th scope="col">Role</th>
                 <th scope="col">Action</th>
                 </tr>
             </thead>
             <tbody>
-              
+              { polls.map((poll, index)=>(
+                <tr>
+                  <td>{index + 1}</td>
+                  <td>{poll.title}</td>
+                  <td><img src={poll.avatar ?? ''} alt="avatar" style={{ width: '50px', height: '50px' }} /></td>
+                  <td>{poll.description}</td>
+                  <td style={{textAlign:"center", margin:"auto" }} > 
+                    <button className="btn btn-primary" style={{marginRight:"5px"}}>Edit</button>
+                    <button className="btn btn-danger" style={{marginLeft:"10px"}}>Delete</button>
+                  </td>
+                </tr>
+              ))}
             </tbody>
         </table>    
     </div>
