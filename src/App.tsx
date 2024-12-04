352acab4d2aaf602c686f96dda62e3c96c964dff
NguyenHoangThai0204 <thaidp2002@gmail.com>
2024-12-04 11:58:46 +0700

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,4 @@
-// import { HashRouter, Routes, Route } from 'react-router-dom';
-// import { HomeLayout } from './component/Home/HomeLayout/HomeLayout';
-// import PrivateRoute from './contextapi/PrivateRoute';
-// import { AuthProvider } from './contextapi/AuthContext';
-// import { FormLayout } from './component/LoginAndRegister/FormLayout/FormLayout';
-
-// function App() {
-//   return (
-//     <AuthProvider>
-//       <HashRouter>
-//         <Routes>
-//           <Route path="/" element={<HomeLayout />} />
-//           <Route element={<PrivateRoute />}>
-//             <Route path="home" element={<HomeLayout />} />
-//             <Route path="poll" element={<HomeLayout />} />
-//             <Route path="personal-page" element={<HomeLayout />} />
-//             <Route path="setting" element={<HomeLayout />} />
-//             <Route path="create-poll" element={<HomeLayout />} />
-//             <Route path="home/getUserByid" element={<HomeLayout />} />
-//           </Route>
-//           <Route path="detail-poll/:id" element={<HomeLayout />} />
-//           <Route path="thenew" element={<HomeLayout />} />
-//           <Route path="logout" element={<HomeLayout />} />
-//           <Route path="login" element={<FormLayout formType={'login'} onLoginClick={function (): void {
-//             throw new Error('Function not implemented.');
-//           }} onSignUpClick={function (): void {
-//             throw new Error('Function not implemented.');
-//           }} onCloseForm={function (): void {
-//             throw new Error('Function not implemented.');
-//           }} />} />
-//         </Routes>
-//       </HashRouter>
-//     </AuthProvider>
-//   );
-// }
-
-// export default App;
-
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import { HomeLayout } from './component/Home/HomeLayout/HomeLayout';
 import PrivateRoute from './contextapi/PrivateRoute';
 import { AuthProvider } from './contextapi/AuthContext';
@@ -45,7 +7,7 @@ import { FormLayout } from './component/LoginAndRegister/FormLayout/FormLayout';
 function App() {
   return (
     <AuthProvider>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<HomeLayout />} />
           <Route element={<PrivateRoute />}>
@@ -67,7 +29,7 @@ function App() {
             throw new Error('Function not implemented.');
           }} />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </AuthProvider>
   );
 }
